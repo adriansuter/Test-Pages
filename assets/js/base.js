343dc1245378ae3b4fc76b8b7a4d0ff59359dd62
adriansuter <adrian@suter-wirz.ch>
2019-05-28 14:32:04 +0200

    Update base.js

--- assets/js/base.js
+++ assets/js/base.js
@@ -1,46 +1,45 @@
-(function (window, document) {
+(function (w, d) {
+  let layout = d.getElementById('layout'),
+    menu = d.getElementById('menu'),
+    menuLink = d.getElementById('menuLink'),
+    content = d.getElementById('main');
 
-    var layout   = document.getElementById('layout'),
-        menu     = document.getElementById('menu'),
-        menuLink = document.getElementById('menuLink'),
-        content  = document.getElementById('main');
+  function toggleClass (element, className) {
+    let classes = element.className.split(/\s+/),
+      length = classes.length,
+      i = 0;
 
-    function toggleClass(element, className) {
-        var classes = element.className.split(/\s+/),
-            length = classes.length,
-            i = 0;
-
-        for(; i < length; i++) {
-          if (classes[i] === className) {
-            classes.splice(i, 1);
-            break;
-          }
-        }
-        // The className is not found
-        if (length === classes.length) {
-            classes.push(className);
-        }
-
-        element.className = classes.join(' ');
+    for (; i < length; i++) {
+      if (classes[i] === className) {
+        classes.splice(i, 1);
+        break;
+      }
+    }
+    // The className is not found
+    if (length === classes.length) {
+      classes.push(className);
     }
 
-    function toggleAll(e) {
-        var active = 'active';
+    element.className = classes.join(' ');
+  }
 
-        e.preventDefault();
-        toggleClass(layout, active);
-        toggleClass(menu, active);
-        toggleClass(menuLink, active);
+  function toggleAll (e) {
+    let active = 'active';
+
+    e.preventDefault();
+    toggleClass(layout, active);
+    toggleClass(menu, active);
+    toggleClass(menuLink, active);
+  }
+
+  menuLink.onclick = function (e) {
+    toggleAll(e);
+  };
+
+  content.onclick = function (e) {
+    if (menu.className.indexOf('active') !== -1) {
+      toggleAll(e);
     }
+  };
 
-    menuLink.onclick = function (e) {
-        toggleAll(e);
-    };
-
-    content.onclick = function(e) {
-        if (menu.className.indexOf('active') !== -1) {
-            toggleAll(e);
-        }
-    };
-
-}(this, this.document));
+}(window, window.document));
